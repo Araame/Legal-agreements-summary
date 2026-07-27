3708cc7bd28930f4363d21717a44d084b3942912
Araame <aram171002@gmail.com>
2026-07-27 13:55:19 +0000

    feat : Classification by stars done

--- ml_service.py
+++ ml_service.py
@@ -4,10 +4,20 @@ from transformers import pipeline
 app = FastAPI()
 
 classifier = pipeline("text-classification", model="nlptown/bert-base-multilingual-uncased-sentiment")
+
+
+def class_by_stars(result):
+    if result[0]["label"] == "1 star" or result[0]["label"] == "2 stars" :
+        status = "negatif" 
+    elif result[0]["label"] == "3 stars":
+        status = "neutre"
+    else :
+        status = "positif" 
+    return status
  
 
 
 @app.post("/analyze-review")
 def classifier_feedbacks(feedback):
     result = classifier(feedback)
-    return {"result": result}
+    return {"result": class_by_stars(result)}
