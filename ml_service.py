5b9b4d5eb221bec63ec65d2c64b9187a39f409ae
Araame <aram171002@gmail.com>
2026-07-28 09:42:27 +0000

    Cached memory for model done

--- ml_service.py
+++ ml_service.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 from transformers import pipeline
+from pydantic import BaseModel, Field
+from functools import lru_cache
 
 app = FastAPI()
 
-classifier = pipeline("text-classification", model="nlptown/bert-base-multilingual-uncased-sentiment")
+@lru_cache(maxsize=1)
+def get_pipeline():
+    classifier = pipeline("text-classification", model="nlptown/bert-base-multilingual-uncased-sentiment")
+    return classifier
 
+
+# class ReviewModel(BaseModel):
+#     content : str = Field(min_length=20, max_length=50)
 
 def class_by_stars(result):
     if result[0]["label"] == "1 star" or result[0]["label"] == "2 stars" :
@@ -14,10 +22,19 @@ def class_by_stars(result):
     else :
         status = "positif" 
     return status
- 
+
+def is_urgent_or_not(status):
+    if status == 'positif' or status == 'neutre':
+        is_urgent = False
+    else :
+        is_urgent = True
+    return {"is_urgent" : is_urgent}
+        
 
 
 @app.post("/analyze-review")
 def classifier_feedbacks(feedback):
-    result = classifier(feedback)
-    return {"result": class_by_stars(result)}
+        classifier = get_pipeline()
+        result = classifier(feedback)
+        status = class_by_stars(result)
+        return {"result": class_by_stars(result),"is_urgent": is_urgent_or_not(status)}
